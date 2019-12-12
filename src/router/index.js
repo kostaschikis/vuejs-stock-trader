4078da61ce7f7aa9a740188380cf2ef2140155ac
Kostas Chikimtzis <kostaschikis@gmail.com>
2019-12-12 23:09:31 +0200

    Added Component Lazy Loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
-import Portfolio from '../components/portfolio/Portfolio.vue'
-import Stocks from '../components/stocks/Stocks.vue'
+// import Portfolio from '../components/portfolio/Portfolio.vue'
+// import Stocks from '../components/stocks/Stocks.vue'
+
+const Portfolio = () => import('../components/portfolio/Portfolio.vue')
+const Stocks = () => import('../components/stocks/Stocks.vue')
 
 Vue.use(VueRouter)
 
